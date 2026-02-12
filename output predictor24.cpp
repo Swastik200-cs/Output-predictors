0b23405d0e0fea91b8a50aad7148f4859f069b6d
Swastik Chaudhuri <swastik.c2020@gmail.com>
2026-02-12 08:12:39 +0530

    Add files via upload

---
+++ output predictor24.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <string>
+using namespace std;
+class Solution {
+  public:
+    string isPrime(int n) {
+        if (n <= 1)
+            return "No";
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return "No"; 
+            }
+        }
+        return "Yes";
+    }
+};
